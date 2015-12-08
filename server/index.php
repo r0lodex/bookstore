b86c87c86e099004073ecc45f28cc55161f7cda8
Irfan Radzi <irfan@spreeo.com>
2015-12-09 00:32:00 +0800

    Reorganized structure and added index content

--- server/index.php
+++ server/index.php
@@ -1,4 +1,11 @@
 <?php
+/*
+
+    1 - Setup static folder using Horus
+    2 - Deny public access to non-static folders
+
+*/
+
 
 require 'Horus.php';
 require_once 'Twig/Autoloader.php';
@@ -10,7 +17,11 @@ function _template($path, $data=null) {
     $loader   = new Twig_Loader_Filesystem('../templates');
     $twig     = new Twig_Environment($loader, array('debug'=>true));
 
+    // Figure out mcm mana nak set global to the template...
+    $ENV["BOOKSTORE_TITLE"] = 'Kedai Buku SIH';
+
     $twig->addExtension(new Twig_Extension_Debug());
+    $twig->addGlobal('env', $ENV);
 
     $template = $twig->loadTemplate($path.'.twig');
 
@@ -18,21 +29,11 @@ function _template($path, $data=null) {
 }
 
 $app->on('GET /', function() {
-    $this->render(_template('index', []));
-});
-
-$app->on('GET /about', function() {
-    $this->render(_template('about', []));
-});
-
-$app->on('GET /contact', function() {
-    $this->render(_template('contact', []));
-});
-
-$app->on('GET /how-to', function() {
-    $this->render(_template('how-to', []));
-});
-
-$app->on('GET /dashboard', function() {
-    $this->render(_template('dashboard', []));
+    $this->end(_template('index', []));
+})->on('GET /contact', function() {
+    $this->end(_template('contact', []));
+})->on('GET /how-to', function() {
+    $this->end(_template('how-to', []));
+})->on('GET /dashboard', function() {
+    $this->end(_template('dashboard', []));
 });
