6678311459a1a2aaf86fb6a9f413a9f5814c8132
Irfan Radzi <irfan@spreeo.com>
2015-12-20 13:25:03 +0800

    Added admin dashboard template

--- client/static/js/_dashboard.js
+++ client/static/js/_dashboard.js
@@ -7,3 +7,46 @@ var dashboard = angular.module('dashboard', [
     'ui.bootstrap',
     'ui-notification'
 ])
+
+.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        controller: 'homeCtrl',
+        templateUrl: '/templates/admin/home.html',
+    });
+
+    $routeProvider.when('/order', {
+        controller: 'orderCtrl',
+        templateUrl: '/templates/admin/order.html',
+    });
+
+    $routeProvider.when('/inventori', {
+        controller: 'inventoriCtrl',
+        templateUrl: '/templates/admin/inventori.html',
+    });
+
+    $routeProvider.when('/pengguna', {
+        controller: 'penggunaCtrl',
+        templateUrl: '/templates/admin/pengguna.html',
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/' });
+})
+
+.config(function(NotificationProvider) {
+    NotificationProvider.setOptions({
+        startTop: 40,
+        positionX: 'right',
+        positionY: 'top'
+    });
+})
+
+.run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeSuccess', function () {
+        $rootScope.currentPath = $location.path();
+    });
+})
+
+.controller('homeCtrl', function($scope) {})
+.controller('orderCtrl', function($scope) {})
+.controller('inventoriCtrl', function($scope) {})
+.controller('penggunaCtrl', function($scope) {})
